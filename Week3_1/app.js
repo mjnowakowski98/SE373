70267a2eff144995115828a14ca432ba4e81295a
Nowakowski <mjnowakowski@labs.neit.edu>
2019-01-28 20:51:53 -0500

    Lab3-1 Complete

--- Week3_1/app.js
+++ Week3_1/app.js
@@ -8,6 +8,21 @@ app.use(express.urlencoded());
 app.use(express.static(__dirname + "/public"));
 
 hbs.registerPartials(__dirname + "/views/partials");
+hbs.registerHelper('colorGrid', (gridSize) => {
+	let output = "<table style='margin:0 auto;'><tbody>";
+	for(let i = 0; i < gridSize; i++) {
+		output += "<tr>";
+		for(let j = 0; j < gridSize; j++) {
+			let color = ((1 << 24) * Math.random()|0).toString(16);
+			output += `<td style="background-color:#${color};">${color}`;
+			output += `<br><span style="color:#ffffff;">${color}</span></td>`;
+		}
+		output += "</tr>";
+	}
+	output += "</tbody></table>"
+
+	return new hbs.SafeString(output);
+});
 
 app.get(["/", "/index"], (req, res) => {
 	res.render("index.hbs", { title:"Color Grid Selector", gridSizes: [3,4,5,10,20] });
@@ -15,22 +30,7 @@ app.get(["/", "/index"], (req, res) => {
 
 app.get("/grid", (req, res) => {
 	let gridSize = req.query["gridSize"] || 0;
-
-	let rows = new Array();
-	for(let i = 0; i < gridSize; i++) {
-		let cols = new Array();
-		for(let j = 0; j < gridSize; j++) {
-			let color = ((1 << 24) * Math.random()|0).toString(16);
-			cols.push(color);
-		}
-		rows.push(cols);
-	}
-
-	res.render("grid.hbs", { title:"The Grid", colorGrid:rows });
-});
-
-app.get('*', (req, res) => {
-	res.render("error.hbs", { errCode:404 });
+	res.render("grid.hbs", { title:"The Grid", gridSize:gridSize });
 });
 
 let appPort = config.server.httpPort;
